3a0387b63396d867360dd2037ced8c519b0ca175
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-02-22 11:20:59 +0100

    fixed error with routes and add icons, logo and config tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignInCandidate from "./pages/SignInCandidate";
+import SignInCompany from "./pages/SignInCompany";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SignInCandidate" element={<SignInCandidate />} />
+        <Route path="/SignInCompany" element={<SignInCompany />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import RightCallButton from "../components/RightCallButton";
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="font-jost">
         Nous cassons les codes du recrutement <span>informatique</span>
       </h1>
       <div>
