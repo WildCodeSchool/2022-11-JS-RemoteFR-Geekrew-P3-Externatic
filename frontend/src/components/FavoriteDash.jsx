f6a35e2bc4b3b9753bf66ba48a1b1ace1dcd9289
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-03-02 12:12:12 +0100

    add favOffer to the dash

--- frontend/src/components/FavoriteDash.jsx
+++ frontend/src/components/FavoriteDash.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import OffersFav from "./OffersFav";
 
 function FavoriteDash() {
   return (
-    <div className="mx-8">
+    <div className="mx-8 mb-8">
       <h1 className="font-jost font-semibold text-xl my-5">
         Vos annonces favorites
       </h1>
-      <div className="bg-white rounded p-4 flex flex-col gap-4">Test div</div>
+      <div className="bg-white rounded p-4 flex flex-col gap-4">
+        <OffersFav />
+      </div>
     </div>
   );
 }
