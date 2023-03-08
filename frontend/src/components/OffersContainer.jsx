61817ec099a8861bac16260e93ebc0194a979ed4
Beldaras <jphilippe.roblot@gmail.com>
2023-03-08 10:59:40 +0100

    fetch job offers, display in progress

--- frontend/src/components/OffersContainer.jsx
+++ frontend/src/components/OffersContainer.jsx
@@ -3,20 +3,30 @@ import axios from "axios";
 import OfferDash from "./OfferDash";
 
 function OffersContainer() {
-  const [jobOffer, setJobOffer] = useState({});
+  const [jobOffers, setJobOffers] = useState([]);
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/job_offers`)
-      .then((res) => setJobOffer(res.data));
-  });
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/job_offers`).then((res) => {
+      setJobOffers(res.data);
+    });
+  }, []);
+
   return (
     <div className="mx-8">
       <h1 className="font-jost font-semibold text-xl mb-5">
         Les offres qui vous correspondent
       </h1>
       <div className="bg-white rounded p-4 flex flex-col gap-4">
-        <OfferDash jobOffer={jobOffer} />
-        <OfferDash jobOffer={jobOffer} />
+        {jobOffers.map((jobOffer) => (
+          <OfferDash
+            key={jobOffer.id}
+            title={jobOffer.title}
+            description={jobOffer.description}
+            location={jobOffer.location}
+            experience={jobOffer.experience}
+            lowerSalary={jobOffer.lower_salary}
+            higherSalary={jobOffer.higher_salary}
+          />
+        ))}
       </div>
     </div>
   );
