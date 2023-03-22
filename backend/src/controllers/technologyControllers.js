ef07961999cf3bcbc2d4cdbe3449c39d01440a99
Beldaras <jphilippe.roblot@gmail.com>
2023-03-22 14:24:10 +0100

    add technologies in backend

---
+++ backend/src/controllers/technologyControllers.js
@@ -0,0 +1,87 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.technology
+    .techName()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.technology
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const technology = req.body;
+
+  technology.id = parseInt(req.params.id, 10);
+
+  return models.user
+    .update(technology)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const technology = req.body;
+
+  return models.technology
+    .insert(technology)
+    .then(([result]) => {
+      res.location(`/technologies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.technology
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
