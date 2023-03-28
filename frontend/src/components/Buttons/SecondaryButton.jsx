3f12b81c3c689c8709434358b5915f141b400b8a
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-03-28 11:55:40 +0200

    quick fix buttons

--- frontend/src/components/Buttons/SecondaryButton.jsx
+++ frontend/src/components/Buttons/SecondaryButton.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 
-function SecondaryButton({ children }) {
+function SecondaryButton({ children, handleClick }) {
   return (
     <button
+      onClick={handleClick}
       type="button"
-      className="flex font-jost justify-center w-full font-semibold text-main underline decoration-2 text-sm mb-6 md:w-1/5"
+      className="flex font-jost justify-center w-full font-semibold text-main underline decoration-2 text-sm mb-6 md:w-1/5 hover:text-main-dark"
     >
       {children}
     </button>
@@ -13,6 +14,11 @@ function SecondaryButton({ children }) {
 
 SecondaryButton.propTypes = {
   children: PropTypes.node.isRequired,
+  handleClick: PropTypes.func,
+};
+
+SecondaryButton.defaultProps = {
+  handleClick: () => {},
 };
 
 export default SecondaryButton;
--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 import SecondaryButton from "../components/Buttons/SecondaryButton";
 
 import thank from "../assets/images/thank.svg";
 
 function Logout() {
+  const navigate = useNavigate();
+  const handleHome = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
   return (
     <div className="flex flex-col items-center mt-5">
       <h1 className="font-jost text-2xl p-5">Vous êtes bien déconnecté !</h1>
-      <SecondaryButton>Revenir à l'accueil</SecondaryButton>
+      <SecondaryButton handleClick={handleHome}>
+        Revenir à l'accueil
+      </SecondaryButton>
       <img src={thank} alt="thank you" className="w-60 h-60 mt-16" />
     </div>
   );
